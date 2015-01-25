abcec63ac7ba1f7a6b299c1c25f8406c78a05adf
Soham Banerjee <soham.pablo@gmail.com>
2015-01-25 17:11:42 -0500

    Created functions for assignment 2
    sohampb - Created functions for assignment 2 to create a cached matrix, and if it doesnt exist to find the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+        varM <- NULL #this variable will later hold the cached matrix
+        getMatrix <- function() x #this function obtains the original matrix whose inverse is to be found
+        setInverseMatrix <- function(invMatrix) varM <<- invMatrix #function to store inverse matrix in varM
+        getInverseMatrix <- function() varM #function to retrieve varM - the inversed matrix
+        
+        # return the list of functions as an R object
+        list(getMatrix=getMatrix, setIinvMatrix=setInvMatrix, getInvMatrix=getInvMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Write a short comment describing this function 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        varM <- x$getInvMatrix()
+        if(!is.null(varM)){
+                message("Found the cached result. Returning the same matrix.")
+                return(varM)
+        }
+        else {
+                message("There was no cache data. Returning inverse matrix")
+                matrixData <- x$getMatrix() # obtains matrix from arguement x
+                varM <- solve(matrixData) # calculate the inverse martix 
+                x$setInvMatrix(varM) # assigns resulting inverse matrix back to object x
+                return(varM)
+        }
 }
